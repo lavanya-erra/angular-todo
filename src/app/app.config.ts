cae68531184d91912ac34fd1032291ac0423e5a0
lavanya.erra <lavanya.erra@yahoo.com>
2024-04-25 08:37:44 -0500

    Adding angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService} from '@angular/fire/analytics';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {getPerformance, providePerformance} from '@angular/fire/performance';
+import {getStorage, provideStorage} from '@angular/fire/storage';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(providePerformance(() => getPerformance())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    "projectId": "lerra-todo",
+    "appId": "1:311555873837:web:e4ff7d6e288c1304b96ca4",
+    "storageBucket": "lerra-todo.appspot.com",
+    "apiKey": "AIzaSyCq8bsGCKxQG23thIC4aORBkbIaP1peirI",
+    "authDomain": "lerra-todo.firebaseapp.com",
+    "messagingSenderId": "311555873837",
+    "measurementId": "G-G1QNW4FXNF"
+  }
+};
